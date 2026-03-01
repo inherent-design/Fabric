32e18bf8124a6cb0acbd558cb2100e59eba229db
mannie-exe <mannie@inherent.design>
2026-02-28 16:59:48 -0800

    feat(sprint-13): save/load UI keybinds and toast notifications (EF-11c)

--- include/fabric/core/InputRouter.hh
+++ include/fabric/core/InputRouter.hh
@@ -4,6 +4,7 @@
 #include <functional>
 #include <RmlUi/Core/Input.h>
 #include <SDL3/SDL.h>
+#include <unordered_map>
 
 namespace Rml {
 class Context;
@@ -38,6 +39,14 @@ class InputRouter {
     // Console toggle callback (backtick key)
     void setConsoleToggleCallback(std::function<void()> cb) { consoleToggleCallback_ = std::move(cb); }
 
+    /// Register a callback that fires on non-repeat key-down events.
+    /// Callbacks are suppressed in UIOnly mode. Replaces any previous
+    /// callback for the same key.
+    void registerKeyCallback(SDL_Keycode key, std::function<void()> cb);
+
+    /// Remove a previously registered key callback.
+    void unregisterKeyCallback(SDL_Keycode key);
+
     // SDL-to-RmlUI key mapping (public for testing)
     static Rml::Input::KeyIdentifier sdlKeyToRmlKey(SDL_Keycode key);
     static int sdlModToRmlMod(SDL_Keymod mod);
@@ -46,6 +55,7 @@ class InputRouter {
     InputManager& inputMgr_;
     InputMode mode_ = InputMode::GameOnly;
     std::function<void()> consoleToggleCallback_;
+    std::unordered_map<SDL_Keycode, std::function<void()>> keyCallbacks_;
 
     bool forwardToRmlUI(const SDL_Event& event, Rml::Context* ctx);
     bool forwardToGame(const SDL_Event& event);
